0610969dbd01fa462083d2ba024d3614905e09c5
Charlie Proctor <charlie@charlieproctor.com>
2015-09-19 01:18:08 -0400

    auth with google

--- src/controllers/sections.js
+++ src/controllers/sections.js
@@ -17,9 +17,19 @@ angular.module('section50.sections', ['ui.router','firebase'])
 	  })
 	}])
 
-.controller('SectionCtrl',['$scope','$state','$firebaseObject',function($scope,$state,$firebaseObject){
+.controller('SectionCtrl',['$scope','$state','$firebaseObject','$firebaseAuth',function($scope,$state,$firebaseObject,$firebaseAuth){
 
 	var ref = new Firebase("https://resplendent-torch-491.firebaseio.com/");
+
+	var auth = $firebaseAuth(ref)
+	
+	// login with Facebook
+  auth.$authWithOAuthPopup("google").then(function(authData) {
+    console.log("Logged in as:", authData.uid);
+  }).catch(function(error) {
+    console.log("Authentication failed:", error);
+  });
+
 	var obj = $firebaseObject(ref.child('sections'))
 
 	obj.$bindTo($scope, "data");
