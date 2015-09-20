35f0ab3d1488779472810caed5fef288aa0abd90
Charlie Proctor <charlie@charlieproctor.com>
2015-09-20 17:22:58 -0400

    adding scope example

--- src/controllers/sections.js
+++ src/controllers/sections.js
@@ -23,6 +23,25 @@ angular.module('section50.sections', ['ui.router','firebase'])
 	}
 	
 }])
-.controller('SectionTwoCtrl',['$scope','$state','$rootScope',function($scope,$state,$rootScope){
-	$scope.name = $rootScope.authData.google.displayName
+.controller('SectionTwoCtrl',['$scope','$state','$rootScope','$firebaseObject','$firebaseArray',
+	function($scope,$state,$rootScope,$firebaseObject,$firebaseArray){
+
+		$scope.name = $rootScope.authData.google.displayName
+		// $scope.hasteam = false
+
+		// var ref = new Firebase("https://resplendent-torch-491.firebaseio.com/").child('teams')
+		// $scope.newTeam = function(){
+		// 	var newref = ref.push()
+		// 	var obj = $firebaseObject(newref)
+		// 	obj.$value = {
+		// 		name : $scope.teamname
+		// 	}
+		// 	obj.$save()
+
+		// 	$scope.hasteam = true
+		// 	$scope.teamuid = obj.$key
+		// }
+
+		// $scope.teams = $firebaseArray(ref)
+
 }])
