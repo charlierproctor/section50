d97513078cecf0a0dd83d5c2036032672d5ac8b0
Charlie Proctor <charlie@charlieproctor.com>
2015-09-20 05:16:48 -0400

    removing /run route

--- server.js
+++ server.js
@@ -8,21 +8,6 @@ app.use(compress());
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist'));
 
-app.post('/run', function(req,res){
-	var options = { 
-		method: 'POST',
-		url: 'http://run.cs50.net:80/run',
-		headers: { 'content-type': 'application/json' },
-		body: req.body,
-		json: true 
-	};
-
-	request(options, function (error, response, body) {
-	  if (error) throw new Error(error);
-	  res.send(body);
-	});
-})
-
 app.get('*', function(req, res){
 	res.sendFile(__dirname + '/dist/index.html');
 })
