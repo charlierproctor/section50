ceeb7d1509f1817a076a316d32b43890c9265f60
Charlie Proctor <charlie@charlieproctor.com>
2015-09-21 02:37:44 -0400

    take in string from command line in commandline.c

--- src/data/problems/2/commandline.c
+++ src/data/problems/2/commandline.c
@@ -5,8 +5,7 @@
 int main(int argc, char* argv[])
 {
 	// Take a string from the command-line.
-	printf("Please give me a string: ");
-	string input = GetString();
+	string input = argv[1];
 
 	// Print out every other letter.
 	for (int i = 0, n = strlen(input); i < n; i+=2) {
